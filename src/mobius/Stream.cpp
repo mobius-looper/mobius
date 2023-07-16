891b96d36b58abfc08cd2947b7cc62b9f9054015
Chris Lunsford <cllunsford@gmail.com>
2023-07-16 08:23:37 -0400

    Simplify deltaFrames diff calculation

--- src/mobius/Stream.cpp
+++ src/mobius/Stream.cpp
@@ -486,8 +486,7 @@ PUBLIC int Stream::getPitchTweak(int tweak)
 
 PUBLIC long Stream::deltaFrames(float* start, float* end)
 {
-	long bytes = (long)((ptrdiff_t)end - (ptrdiff_t)start);
-	long samples = bytes / sizeof(float);
+	ptrdiff_t samples = end - start;
 	long frames = samples / channels;
 	return frames;
 }
